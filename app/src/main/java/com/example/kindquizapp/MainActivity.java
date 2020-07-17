5a40d18e63cce769f023d8f560d901d70260df66
Rudra kaniya <rudra.kaniya.rk@gmail.com>
2020-07-18 01:05:00 +0530

    Adding the QuestionChecker functionality

--- app/src/main/java/com/example/kindquizapp/MainActivity.java
+++ app/src/main/java/com/example/kindquizapp/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -21,28 +22,44 @@ public class MainActivity extends AppCompatActivity {
 
     Button mSubmitButton;
     Button mNextButton;
+    TextView mQuestionTextView;
+    int mIndex;
+    int mQuestion;
 
+    // The question Bank for quiz
+    private QuestionChecker[] mQuestionBank = new QuestionChecker[] {
+            new QuestionChecker( R.string.mQuestion1, R.string.mAnswer1),
+            new QuestionChecker( R.string.mQuestion2, R.string.mAnswer2),
+            new QuestionChecker( R.string.mQuestion3, R.string.mAnswer3),
+            new QuestionChecker( R.string.mQuestion4, R.string.mAnswer4),
+            new QuestionChecker( R.string.mQuestion5, R.string.mAnswer5),
+            new QuestionChecker( R.string.mQuestion6, R.string.mAnswer6),
+            new QuestionChecker( R.string.mQuestion7, R.string.mAnswer7),
+            new QuestionChecker( R.string.mQuestion8, R.string.mAnswer8),
+            new QuestionChecker( R.string.mQuestion9, R.string.mAnswer9),
+            new QuestionChecker( R.string.mQuestion10, R.string.mAnswer10),
+            new QuestionChecker( R.string.mQuestion11, R.string.mAnswer11),
+            new QuestionChecker( R.string.mQuestion12, R.string.mAnswer12),
+            new QuestionChecker( R.string.mQuestion13, R.string.mAnswer13)
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // For selecting option A
+        // For selecting option buttons
         mAnswerA = findViewById(R.id.answer_A);
-
-        // For selecting option B
         mAnswerB = findViewById(R.id.answer_B);
-
-        // For selecting option C
         mAnswerC = findViewById(R.id.answer_C);
-
-        // For selecting option D
         mAnswerD = findViewById(R.id.answer_D);
 
         // Submit button and the Next button
         mSubmitButton = findViewById(R.id.submitButton);
         mNextButton = findViewById(R.id.nextButton);
+
+        mQuestion = mQuestionBank[mIndex].getQuestionID();
+        mQuestionTextView.setText(mQuestion);
 
         mAnswerA.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -94,6 +111,27 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(), "Next Button Pressed!!", Toast.LENGTH_SHORT).show();
             }
         });
-
     }
+
+    private void updateQuestion(){
+        mIndex = (mIndex + 1 );
+
+        if (mIndex >= mQuestionBank.length){
+            mIndex = mIndex % mQuestionBank.length;
+        }
+        mQuestion = mQuestionBank[mIndex].getQuestionID();
+        mQuestionTextView.setText(mQuestion);
+    }
+
+    private void checkAnswer(int userSelection) {
+        int correctAnswer = mQuestionBank[mIndex].isAnswer();
+
+        if (userSelection == correctAnswer){
+            Toast.makeText(getApplicationContext(), R.string.correct_toast, Toast.LENGTH_SHORT);
+        }
+        else {
+            Toast.makeText(getApplicationContext(), R.string.incorrect_toast, Toast.LENGTH_SHORT);
+        }
+    }
+
 }
--- app/src/main/java/com/example/kindquizapp/QuestionChecker.java
+++ app/src/main/java/com/example/kindquizapp/QuestionChecker.java
@@ -2,10 +2,30 @@ package com.example.kindquizapp;
 
 public class QuestionChecker {
     private int mQuestionID;
-    private String mAnswer;
+    private int mAnswer;
 
-    public QuestionChecker(int questionresourceID, String correctAnswer){
-        mQuestionID = questionresourceID;
+    public QuestionChecker(int questionResourceID, int correctAnswer){
+        mQuestionID = questionResourceID;
         mAnswer = correctAnswer;
+
+        // I am typing this text from my new keyboard, which i have just installed in my new laptop table
+        // I am feeling good vibes by typing from my this new keyboard.
+
+    }
+
+    public int getQuestionID(){
+        return mQuestionID;
+    }
+
+    public void setQuestionID(int questionID){
+        mQuestionID = questionID;
+    }
+
+    public int isAnswer() {
+        return mAnswer;
+    }
+
+    public void getQuestionID(int answer){
+        mAnswer = answer;
     }
 }
